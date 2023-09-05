3e05c904025c56072b6c8917539b5606b07dde29
Nicolai Blak Walther <niwal19@student.sdu.dk>
2023-09-05 11:52:28 +0200

    Template cleanup and api-routes implemented

--- src/services/database/database.d.ts
+++
@@ -1,9 +0,0 @@
-import { ExampleUser } from "@prisma/client";
-
-export type DatabaseHook = {
-    exampleUsers: ExampleUser[];
-    createExampleUser: (name: string, email: string) => Promise<void>;
-    getExampleUsers: () => Promise<void>;
-    updateExampleUser: (id: string, name: string) => Promise<void>;
-    deleteExampleUser: (id: string) => Promise<void>;
-}
--- src/services/database/useDbContextValue.ts
+++ src/services/database/useDbContextValue.ts
@@ -1,54 +1,26 @@
 import { useCallback, useState } from "react";
-import { DatabaseHook } from "./database";
-import { ExampleUser } from "@prisma/client";
+import { Beer } from "@prisma/client";
 import { genApiClient } from "../backend/appApiClient";
 
+export type DatabaseHook = {
+	beers: Beer[];
+	getAllBeers: () => Promise<void>;
+};
+
 export const useDbContextValue = (): DatabaseHook => {
-    const [exampleUsers, setExampleUsers] = useState<ExampleUser[]>([]);
+	const [beers, setBeer] = useState<Beer[]>([]);
 
-    const createExampleUser = useCallback(
-        async (name: string, email: string) => {
-            const client = await genApiClient();
+	const getAllBeers = useCallback(async (): Promise<void> => {
+		const client = await genApiClient();
+		const ex: any = await client.getRequest("beer");
 
-            const newUser = { name, email };
-            const usr: ExampleUser = await client.postRequest("exampleuser", newUser) as ExampleUser;
+		const beers: Beer[] = ex.beers;
 
-            setExampleUsers([...exampleUsers, usr])
-        }, []
-    );
+		setBeer(beers);
+	}, [setBeer]);
 
-    const getExampleUsers = useCallback(
-        async (): Promise<void> => {
-            const client = await genApiClient();
-            const ex: any = await client.getRequest("exampleuser");
-            
-            const users: ExampleUser[] = ex.users;
-
-            setExampleUsers(users);
-        }, []
-    );
-
-    const updateExampleUser = useCallback(
-        async (id: string, newName: string): Promise<void> => {
-            const update = { id, newName };
-
-            const client = await genApiClient();
-            await client.putRequest("exampleuser", update);
-
-            await getExampleUsers();
-        }, []
-    );
-
-    const deleteExampleUser = useCallback(
-        async (id: string) => {
-            const del = { id };
-
-            const client = await  genApiClient();
-            await client.postRequest("deleteuser", del)
-
-            await getExampleUsers();
-        }, []
-    );
-
-    return { exampleUsers, createExampleUser, getExampleUsers, updateExampleUser, deleteExampleUser };
-}
+	return {
+		beers,
+		getAllBeers,
+	};
+};
