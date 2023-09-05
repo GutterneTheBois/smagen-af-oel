56e4c2f5353fbc02f69d54ae20367eed0a65b9c4
Mikkel Dolleris <mikch19@student.sdu.dk>
2023-09-05 10:44:00 +0200

    Updated schema.prisma & seed to include destilleries and spirits

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -2,57 +2,93 @@ import { PrismaClient } from "@prisma/client";
 import breweryData from "./seed-data/breweries.json" assert { type: "json" };
 import beerData from "./seed-data/beers.json" assert { type: "json" };
 
-
 const prisma = new PrismaClient();
 
 async function main() {
-	const breweries = await prisma.$transaction(
-		breweryData.map((brewery) => 
-			prisma.brewery.upsert({
-				where: { name: brewery.name },
-				update: {},
-				create: {
-					name: brewery.name,
-					description: brewery.description
-				}
-			})	
-		)
-	);
-	
-	
-	const beers = await prisma.$transaction(
-		beerData.map((beer) =>
-			prisma.beer.upsert({
-				where: { name: beer.name },
-				update: {},
-				create: {
-					name: beer.name,
-					description: beer.description,
-					type: beer.type,
-					vol: beer.vol,
-					ibu: beer.ibu,
-					size: beer.size,
-					image_url: beer.image_url,
-					brewery: {
-						connect: {
-							name: beer.breweryName
-						}
-					}
-				}
-			})
-		)
-	);
+  const breweries = await prisma.$transaction(
+    breweryData.map((brewery) =>
+      prisma.brewery.upsert({
+        where: { name: brewery.name },
+        update: {},
+        create: {
+          name: brewery.name,
+          description: brewery.description,
+        },
+      })
+    )
+  );
 
-	console.info(`Seeded ${breweries.length} breweries`);
-	console.info(`Seeded ${beers.length} beers`);
+  const beers = await prisma.$transaction(
+    beerData.map((beer) =>
+      prisma.beer.upsert({
+        where: { name: beer.name },
+        update: {},
+        create: {
+          name: beer.name,
+          description: beer.description,
+          type: beer.type,
+          vol: beer.vol,
+          ibu: beer.ibu,
+          size: beer.size,
+          image_url: beer.image_url,
+          brewery: {
+            connect: {
+              name: beer.breweryName,
+            },
+          },
+        },
+      })
+    )
+  );
+
+  const destilleries = await prisma.$transaction(
+    destilleryData.map((destillery) =>
+      prisma.destillery.upsert({
+        where: { name: destillery.name },
+        update: {},
+        create: {
+          name: destillery.name,
+          description: destillery.description,
+        },
+      })
+    )
+  );
+
+  const spirits = await prisma.$transaction(
+    spiritData.map((spirit) =>
+      prisma.spirit.upsert({
+        where: { name: spirit.name },
+        update: {},
+        create: {
+          name: spirit.name,
+          description: spirit.description,
+          type: spirit.type,
+          vol: spirit.vol,
+          ibu: spirit.ibu,
+          size: spirit.size,
+          image_url: spirit.image_url,
+          destillery: {
+            connect: {
+              name: spirit.destilleryName,
+            },
+          },
+        },
+      })
+    )
+  );
+
+  console.info(`Seeded ${breweries.length} breweries`);
+  console.info(`Seeded ${beers.length} beers`);
+  console.info(`Seeded ${destilleries.length} destilleries`);
+  console.info(`Seeded ${spirits.length} spirits`);
 }
 
 main()
-	.then(async () => {
-		prisma.$disconnect();
-	})
-	.catch(async (err) => {
-		console.error(err);
-		await prisma.$disconnect();
-		process.exit(1);
-	});
+  .then(async () => {
+    prisma.$disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
