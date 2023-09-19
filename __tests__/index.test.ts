d99785e000d5ae8ea76c86d6a99acd9b22b9e452
Mikkel Dolleris <mikch19@student.sdu.dk>
2023-09-19 13:50:21 +0200

    Changed index.test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,15 +4,9 @@ import { genApiClient } from "../src/services/backend/appApiClient";
 test("should create new brewery and match", async () => {
   const client = await genApiClient();
 
-  const res: any = (await client.postRequest("brewery", {
-    name: "Walther A/S",
-    description: "Det her er en test",
-  })) as Brewery;
+  const response: any = await client.getRequest("brewery");
 
-  await expect(res).resolves.toEqual({
-    name: "Walther A/S",
-    description: "Det her er en test",
-  });
+  await expect(response).resolves.toEqual(200);
 });
 
 // test("should create new brewery and not match", async () => {
