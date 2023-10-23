2524122babbb94affbb6312cb91abaf9766120d8
Nicolai Blak Walther <niwal19@student.sdu.dk>
2023-10-23 11:18:18 +0200

    Age-cookie implemented

--- src/client-components/Modal/Modal.tsx
+++ src/client-components/Modal/Modal.tsx
@@ -4,17 +4,18 @@ import styles from "./modal.module.scss";
 import ExampleButton from "../Button/Button";
 import GhostButton from "../GhostButton/GhostButton";
 import { IoBeer, IoLogOut } from "react-icons/io5";
+import { createAgeCookie } from "@/services/actions";
 
 const Modal: FC = () => {
 	const [open, setOpen] = useState<boolean>(true);
 
-	const toggle = () => {
+	const toggle = async () => {
 		setOpen(!open);
+		await createAgeCookie();
 	};
 
 	return (
 		<>
-			<ExampleButton onClick={toggle}>SHOW MODAL</ExampleButton>
 			<div
 				className={`${styles.modalContainer} ${
 					open ? `${styles.open}` : `${styles.closed}`
@@ -30,11 +31,12 @@ const Modal: FC = () => {
 					<div className={styles.modalFooter}>
 						<ExampleButton
 							className={styles.modalFooterBtn}
-							onClick={toggle}
+							onClick={async () => await toggle()}
 						>
 							<IoBeer className={styles.modalIcon} />
 							Confirm
 						</ExampleButton>
+
 						<GhostButton className={styles.modalFooterBtn}>
 							<IoLogOut className={styles.modalIcon} />
 							Cancel
