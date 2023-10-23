6027bfdd6efca5712c8596c4f8a11dead0d64233
Mikkel Dolleris <mikch19@student.sdu.dk>
2023-10-23 20:53:00 +0200

    Test of updating brewery created
    Still missing the actual update

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,29 +1,69 @@
-import { Brewery, Prisma } from "@prisma/client";
+import { Brewery } from "@prisma/client";
 import { genApiClient } from "../src/services/backend/appApiClient";
-import { initScriptLoader } from "next/script";
+import { dbClient } from "../src/services/backend/dbClient";
 
-beforeAll(async () => {});
+beforeAll(async () => {
+  const client = await genApiClient();
+
+  await client.postRequest("brewery", {
+    name: "Dolleris A/S",
+    description: "Det her er en test",
+  });
+});
+
+afterEach(async () => {
+  const dbHandler = await dbClient();
+
+  const testBrewery = await dbHandler.findSpecificElement(
+    "brewery",
+    "Dolleris A/S"
+  );
+
+  console.log(testBrewery.description);
+});
 
 it("should make GET request for breweries and receive status 200", async () => {
   const client = await genApiClient();
 
   const response: any = await client.getRequest("brewery");
 
-  console.log(response);
-
   expect(response.status).toEqual(200);
 });
 
-// test("should create new brewery and not match", async () => {
-//   const client = await genApiClient();
+it("should create new brewery and match", async () => {
+  const client = await genApiClient();
 
-//   const res: any = (await client.postRequest("brewery", {
-//     name: "Dolleris A/S",
-//     description: "Det her er en fejl test",
-//   })) as Brewery;
+  const res: any = await client.getRequest("brewery");
 
-//   await expect(res).resolves.toEqual({
-//     name: "Walther A/S",
-//     description: "Det her er en test",
-//   });
-// });
+  const data = await res.json();
+
+  expect(
+    data.breweries.findLast(
+      (brewery: Brewery) => brewery.name === "Dolleris A/S"
+    )
+  );
+});
+
+it("should update brewery and match", async () => {
+  const client = await genApiClient();
+
+  const dbHandler = await dbClient();
+
+  const testBrewery = await dbHandler.findSpecificElement(
+    "brewery",
+    "Dolleris A/S"
+  );
+
+  await client.putRequest(testBrewery.id, "Opdateret beskrivelse");
+
+  const updatedBrewery = await dbHandler.findSpecificElement(
+    "brewery",
+    "Dolleris A/S"
+  );
+
+  console.log(updatedBrewery.description);
+
+  console.log(await client.getRequest("brewery").then((res) => res.json()));
+
+  expect(testBrewery.description === "Opdateret beskrivelse");
+});
