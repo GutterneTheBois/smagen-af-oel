0927762a584d7ee9715ead9501e8f2c53418edf3
Nicolai Blak Walther <niwal19@student.sdu.dk>
2023-11-10 09:32:57 +0100

    Index tests working

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,67 +3,92 @@ import { genApiClient } from "../src/services/backend/appApiClient";
 import { dbClient } from "../src/services/backend/dbClient";
 
 beforeAll(async () => {
-  const client = await genApiClient();
+	const client = await genApiClient();
 
-  await client.postRequest("brewery", {
-    name: "Dolleris A/S",
-    description: "Det her er en test",
-  });
+	await client.postRequest("brewery", {
+		name: "Dolleris A/S",
+		description: "Det her er en test",
+	});
 });
 
 afterEach(async () => {
-  const dbHandler = await dbClient();
+	const dbHandler = await dbClient();
 
-  const testBrewery = await dbHandler.findSpecificElement(
-    "brewery",
-    "Dolleris A/S"
-  );
+	const testBrewery = await dbHandler.findSpecificElement(
+		"brewery",
+		"Dolleris A/S"
+	);
 
-  console.log(testBrewery.description);
+	//console.log(testBrewery.description);
 });
 
 it("should make GET request for breweries and receive status 200", async () => {
-  const client = await genApiClient();
+	const client = await genApiClient();
 
-  const response: any = await client.getRequest("brewery");
+	const response: any = await client.getRequest("brewery");
 
-  expect(response.status).toEqual(200);
+	expect(response.status).toEqual(200);
 });
 
 it("should create new brewery and match", async () => {
-  const client = await genApiClient();
+	const client = await genApiClient();
 
-  const res: any = await client.getRequest("brewery");
+	const res: any = await client.getRequest("brewery");
 
-  const data = await res.json();
+	const data = await res.json();
 
-  expect(
-    data.breweries.findLast(
-      (brewery: Brewery) => brewery.name === "Dolleris A/S"
-    )
-  );
+	expect(
+		data.breweries.findLast(
+			(brewery: Brewery) => brewery.name === "Dolleris A/S"
+		)
+	);
 });
 
 it("should update brewery and match", async () => {
-  const client = await genApiClient();
+	const client = await genApiClient();
 
-  const dbHandler = await dbClient();
+	const dbHandler = await dbClient();
 
-  const testBrewery = await dbHandler.findSpecificElement(
-    "brewery",
-    "Dolleris A/S"
-  );
+	const testBrewery = await dbHandler.findSpecificElement(
+		"brewery",
+		"Dolleris A/S"
+	);
 
-  await client.putRequest(testBrewery.id, "Opdateret beskrivelse");
+	await client.putRequest("brewery", {
+		id: testBrewery.id,
+		newDescription: "Opdateret beskrivelse",
+	});
 
-  const updatedBrewery = await dbHandler.findSpecificElement(
-    "brewery",
-    "Dolleris A/S"
-  );
+	console.log(testBrewery.description);
 
-  console.log(updatedBrewery.description);
+	console.log(await client.getRequest("brewery").then((res) => res.json()));
 
-  console.log(await client.getRequest("brewery").then((res) => res.json()));
+	const updatedBrewery = await dbHandler.findSpecificElement(
+		"brewery",
+		"Dolleris A/S"
+	);
 
-  expect(testBrewery.description === "Opdateret beskrivelse");
+	expect(updatedBrewery.description).toBe("Opdateret beskrivelse");
+});
+
+it("should delete brewery", async () => {
+	const client = await genApiClient();
+
+	const dbHandler = await dbClient();
+
+	const testBrewery = await dbHandler.findSpecificElement(
+		"brewery",
+		"Dolleris A/S"
+	);
+
+	await client.postRequest("brewery/delete", {
+		id: testBrewery.id,
+	});
+
+	const shouldBeNull = await dbHandler.findSpecificElement(
+		"brewery",
+		"Dolleris A/S"
+	);
+
+	expect(shouldBeNull).toBeNull();
 });
