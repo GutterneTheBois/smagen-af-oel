01e28dbaa6f9ccb493db5abe96b091594df3bf2a
Mikkel Dolleris <mikch19@student.sdu.dk>
2023-11-16 11:47:07 +0100

    Changed get all breweries test to match usecase

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,69 +1,76 @@
 import { Brewery } from "@prisma/client";
 import { genApiClient } from "../src/services/backend/appApiClient";
 import { dbClient } from "../src/services/backend/dbClient";
+import breweries from "../prisma/seed-data/breweries.json";
+import { before } from "node:test";
 
-beforeAll(async () => {
-	const client = await genApiClient();
+describe("Test of Brewery API functions", () => {
+	before(async () => {
+		const client = await genApiClient();
 
-	await client.postRequest("brewery", {
-		name: "Test",
-		description: "Det her er en test",
-	});
-});
-
-it("should make GET request for breweries and receive status 200", async () => {
-	const client = await genApiClient();
-
-	const response: any = await client.getRequest("brewery");
-
-	expect(response.status).toEqual(200);
-});
-
-it("should create new brewery and match", async () => {
-	const client = await genApiClient();
-
-	const res: any = await client.getRequest("brewery");
-
-	const data = await res.json();
-
-	expect(
-		data.breweries.findLast((brewery: Brewery) => brewery.name === "Test")
-	);
-});
-
-it("should update brewery and match", async () => {
-	const client = await genApiClient();
-
-	const dbHandler = await dbClient();
-
-	const testBrewery = await dbHandler.findSpecificElement("brewery", "Test");
-
-	await client.patchRequest("brewery", {
-		id: testBrewery.id,
-		newDescription: "Opdateret beskrivelse",
+		await client.postRequest("brewery", {
+			name: "Test",
+			description: "Det her er en test",
+		});
 	});
 
-	console.log(testBrewery.description);
+	it("should make GET request for breweries and match the Schema .json file", async () => {
+		const client = await genApiClient();
 
-	console.log(await client.getRequest("brewery").then((res) => res.json()));
+		const response: any = await client.getRequest("brewery");
 
-	const updatedBrewery = await dbHandler.findSpecificElement("brewery", "Test");
+		const data = await response.json();
 
-	expect(updatedBrewery.description).toBe("Opdateret beskrivelse");
-});
-
-it("should delete brewery", async () => {
-	const client = await genApiClient();
-
-	const dbHandler = await dbClient();
-
-	const testBrewery = await dbHandler.findSpecificElement("brewery", "Test");
-
-	await client.postRequest("brewery/delete", {
-		id: testBrewery.id,
+		expect(data.breweries("name") + data.breweries("description")).toEqual(
+			breweries
+		);
 	});
 
-	const shouldBeNull = await dbHandler.findSpecificElement("brewery", "Test");
+	it("should create new brewery and match", async () => {
+		const client = await genApiClient();
 
-	expect(shouldBeNull).toBeNull();
+		const res: any = await client.getRequest("brewery");
+
+		const data = await res.json();
+
+		expect(
+			data.breweries.findLast((brewery: Brewery) => brewery.name === "Test")
+		);
+	});
+
+	it("should update brewery and match", async () => {
+		const client = await genApiClient();
+
+		const dbHandler = await dbClient();
+
+		const testBrewery = await dbHandler.findSpecificElement("brewery", "Test");
+
+		await client.patchRequest("brewery", {
+			id: testBrewery.id,
+			newDescription: "Opdateret beskrivelse",
+		});
+
+		const updatedBrewery = await dbHandler.findSpecificElement(
+			"brewery",
+			"Test"
+		);
+
+		expect(updatedBrewery.description).toBe("Opdateret beskrivelse");
+	});
+
+	it("should delete brewery", async () => {
+		const client = await genApiClient();
+
+		const dbHandler = await dbClient();
+
+		const testBrewery = await dbHandler.findSpecificElement("brewery", "Test");
+
+		await client.postRequest("brewery/delete", {
+			id: testBrewery.id,
+		});
+
+		const shouldBeNull = await dbHandler.findSpecificElement("brewery", "Test");
+
+		expect(shouldBeNull).toBeNull();
+	});
 });
